7a927c44a963a0d2cfe8c8b49e0352f6f23122af
Younus Khan <younuskhan99@hotmail.com>
2022-06-01 12:01:23 +0100

    2nd challenge complete

--- main.js
+++ main.js
@@ -3,21 +3,21 @@
 // 1. Practice Syntax
 
 const changeToCapital = (arr) => {
-    const newArr = []
-    arr.forEach(item => {
-        newArr.push(item.charAt(0).toUpperCase() + item.substring(1))
+    return arr.map(item => {
+        return item.charAt(0).toUpperCase() + item.substring(1)
     });
-    return newArr
+    
 }
 console.log(changeToCapital(["charlie", "rob"]));
 
 const removeVowels = (arr) => {
-    arr.filter(letter => {
+    return arr.filter(letter => {
         if (letter === "a" || letter === "e" || letter === "i" || letter === "o" || letter === "u") {
-           arr.pop(letter)
+            return false
+        } else {
+            return true
         }
     })
-    return arr
 }
 console.log(removeVowels(["c", "r", "a"]));
 
@@ -38,8 +38,35 @@ const getLongWords = (arr) => {
 console.log(getLongWords(["spray", "limit", "disco", "exuberant", "destruction", "present"]));
 
 const getCamelCasing = (arr) => {
-    arr.forEach(string => {
-        console.log(string.split(" ")); 
+    return arr.map(string => {
+        let subArr = string.split(" ");
+        for (let i = 1; i < subArr.length; i++) {
+            camelCasedWord = subArr[i].charAt(0).toUpperCase() + subArr[i].substring(1)
+            return subArr[0] + camelCasedWord
+        }
     });
 }
 console.log(getCamelCasing(["please camel", "join casing", "these disco", "with shoes"]));
+
+const calculateScrabblePoints = (array) => {
+    console.log(array);
+    let totalNum = 0
+    array.forEach(item => {
+        // console.log(item.value);
+        totalNum = totalNum + item.value
+        // return totalNum
+    })
+    return totalNum
+}
+console.log(calculateScrabblePoints([{ letter: "a", value: 1}, { letter: "k", value: 5}, { letter: "c", value: 3}]));
+
+// 3. Array's and Objects
+
+const convertMilesToKm = (arr) => {
+    let kmObject = {};
+    convertedKM
+    arr.forEach(item => {
+
+    })
+}
+console.log(convertMilesToKm([10, 5]));
